6d4baabf938b68b8cf98de520e3bbb497033186e
Grayson <134082858+GraysonMarks@users.noreply.github.com>
2023-07-26 23:58:21 -0400

    Update

--- include/ShockPot Driver.h
+++ include/ShockPot Driver.h
@@ -1,35 +1,27 @@
+#pragma once
 
-
-
-
+#ifndef SHOCKPOT_DRIVER.H
+#define SHOCKPOT_DRIVER.H 
 
 
 typedef struct 
 {
-
     int val = 0;
     int pos = 0;
-
 } ShockPot;
-
 
 
 class ShockPotDriver
 {
 private:
-
     ShockPot pot[4];
-
 public:
-
     void setReading();
-
     int  getReading();
-
     void getPosition();
-
-
 };
 
-
 static ShockPotDriver ShockPot_;
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 
-
 #include "ShockPot Driver.h"
 #include <Arduino.h>
-
 
 
 void setup()
@@ -12,5 +10,9 @@ void setup()
 
 void loop()
 {
+  // Take reading of Shock Pots
 	ShockPot_.setReading();
+
+  // Print readings to Serial
+  ShockPot_.getReading();
 }
