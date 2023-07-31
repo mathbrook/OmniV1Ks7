273411a46a34124849d75099f302942a3e901d60
jbinvents <55504899+jbinvents@users.noreply.github.com>
2023-07-31 00:52:23 -0400

    SD Card File creating

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,139 @@
 #include <Arduino.h>
-/*
-  Blink
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <SD.h>
+#include <Metro.h>
+#include <TimeLib.h>
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
 int Steering = 14;
 int SteeringVal;
+File logger;
+uint64_t last_sec_epoch;
+
+
+Adafruit_LSM9DS1 lsm = Adafruit_LSM9DS1();
+
+//#define LSM9DS1_SCL = 19
+//#define LSM9DS1_SDA = 18
+
+void setupSensor()
+{
+  // 1.) Set the accelerometer range
+  lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_2G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_4G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_8G);
+  //lsm.setupAccel(lsm.LSM9DS1_ACCELRANGE_16G);
+  
+  // 2.) Set the magnetometer sensitivity
+  lsm.setupMag(lsm.LSM9DS1_MAGGAIN_4GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_8GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_12GAUSS);
+  //lsm.setupMag(lsm.LSM9DS1_MAGGAIN_16GAUSS);
+
+  // 3.) Setup the gyroscope
+  lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_245DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_500DPS);
+  //lsm.setupGyro(lsm.LSM9DS1_GYROSCALE_2000DPS);
+}
+time_t getTeensy3Time() {
+    return Teensy3Clock.get();
+}
+void sd_date_time(uint16_t* date, uint16_t* time) {
+    // return date using FAT_DATE macro to format fields
+    *date = FAT_DATE(year(), month(), day());
+    // return time using FAT_TIME macro to format fields
+    *time = FAT_TIME(hour(), minute(), second());
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(Steering, INPUT);
   Serial.begin(9600);
+  while (!Serial) {
+    delay(1); // will pause Zero, Leonardo, etc until serial console opens
+  }
+   
+  
+ //Set up real-time clock 
+    //Teensy3Clock.set(1660351622); // set time (epoch) at powerup  (COMMENT OUT THIS LINE AND PUSH ONCE RTC HAS BEEN SET!!!!)
+    setSyncProvider(getTeensy3Time); // registers Teensy RTC as system time
+    if (timeStatus() != timeSet) {
+        Serial.println("RTC not set up - uncomment the Teensy3Clock.set() function call to set the time");
+    } else {
+        Serial.println("System time set to RTC");
+    }
+    last_sec_epoch = Teensy3Clock.get();
+
+    Serial.println("Initializing SD card...");
+    SdFile::dateTimeCallback(sd_date_time); // Set date/time callback function
+    SD.begin(BUILTIN_SDCARD);
+    if (!SD.begin(BUILTIN_SDCARD)) { // Begin Arduino SD API (Teensy 3.5)
+        Serial.println("SD card failed or not present");
+    }
+    
+    char filename[] = "data0000.CSV";
+    for (uint8_t i = 0; i < 10000; i++) {
+        filename[4] = i / 1000     + '0';
+        filename[5] = i / 100 % 10 + '0';
+        filename[6] = i / 10  % 10 + '0';
+        filename[7] = i       % 10 + '0';
+        if (!SD.exists(filename)) {
+            Serial.println("Writing SD card File Name");
+            logger = SD.open(filename, (uint8_t) O_WRITE | (uint8_t) O_CREAT); // Open file for writing
+            break;
+        }
+        if (i == 9999) { // If all possible filenames are in use, print error
+            Serial.println("All possible SD card log filenames are in use - please clean up the SD card");
+        }
+    }
+
+
+  Serial.println("LSM9DS1 data read test");
+  
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM9DS1. Check your wiring!");
+    while (1);
+  }
+  Serial.println("Found LSM9DS1 9DOF");
+
+  // helper to just set the default scaling we want, see above!
+  setupSensor();
 }
 
 // the loop function runs over and over again forever
-void loop() {
+void loop()
+{
 
   SteeringVal = analogRead(Steering);
   Serial.println(SteeringVal);
   delay(50);
+
+   lsm.read();  /* ask it to read in the data */ 
+
+  /* Get a new sensor event */ 
+  sensors_event_t a, m, g, temp;
+
+  lsm.getEvent(&a, &m, &g, &temp); 
+
+  Serial.print("Accel X: "); Serial.print(a.acceleration.x); Serial.print(" m/s^2");
+  Serial.print("\tY: "); Serial.print(a.acceleration.y);     Serial.print(" m/s^2 ");
+  Serial.print("\tZ: "); Serial.print(a.acceleration.z);     Serial.println(" m/s^2 ");
+
+  Serial.print("Mag X: "); Serial.print(m.magnetic.x);   Serial.print(" uT");
+  Serial.print("\tY: "); Serial.print(m.magnetic.y);     Serial.print(" uT");
+  Serial.print("\tZ: "); Serial.print(m.magnetic.z);     Serial.println(" uT");
+
+  Serial.print("Gyro X: "); Serial.print(g.gyro.x);   Serial.print(" rad/s");
+  Serial.print("\tY: "); Serial.print(g.gyro.y);      Serial.print(" rad/s");
+  Serial.print("\tZ: "); Serial.print(g.gyro.z);      Serial.println(" rad/s");
+
+  Serial.println("running");
+  delay(200);
   
   //digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   //delay(500);                      // wait for a second
